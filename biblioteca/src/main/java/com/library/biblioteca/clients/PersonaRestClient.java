d4143166c04046095eb4378ad108e5b64a269aca
113875-Comba-Carolina <129430013+113875-Comba-Carolina@users.noreply.github.com>
2025-02-02 20:07:12 -0300

    c

--- biblioteca/src/main/java/com/library/biblioteca/clients/PersonaRestClient.java
+++ biblioteca/src/main/java/com/library/biblioteca/clients/PersonaRestClient.java
@@ -1,13 +1,16 @@
 package com.library.biblioteca.clients;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.library.biblioteca.dto.ClienteDTO;
 
+
+@Service
 public class PersonaRestClient {
     RestTemplate restTemplate = new RestTemplate();
 
-    String url = "http://localhost:8080/api/personas/aleatorio";
+    String url = "http://clientes-service:8081/api/personas/aleatorio";
     
     public ClienteDTO getPersonaAleatoria() {
         return restTemplate.getForObject(url, ClienteDTO.class);
